1e81f7a2284be12b7159c8622ad35080fbef3eae
quanosek <kubaklalo@gmail.com>
2023-12-30 20:29:48 +0100

    show points amount at the bottom of board

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import localFont from "next/font/local";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import styles from "./page.module.scss";
 
@@ -15,6 +15,8 @@ export default function Board({ params }: { params: { id: number } }) {
 
   const [answers, setAnswers] = useState<any>();
   const [show, setShow] = useState<Array<number>>([]);
+
+  const pointsAmount = useRef(0);
 
   // set answers
   useEffect(() => {
@@ -31,61 +33,86 @@ export default function Board({ params }: { params: { id: number } }) {
 
       if ([1, 2, 3, 4, 5, 6].includes(+event.key)) {
         const number = Number(event.key);
-        if (!show.includes(number)) setShow([...show, number]);
+
+        if (!show.includes(number)) {
+          pointsAmount.current += answers[number - 1].points;
+          setShow([...show, number]);
+        }
       }
 
-      if (event.key.toUpperCase() === "ESCAPE") window.close();
+      if (event.key === "Escape") window.close();
     };
 
     document.addEventListener("keyup", KeyupEvent);
     return () => document.removeEventListener("keyup", KeyupEvent);
-  }, [show]);
+  }, [answers, show]);
+
+  const numberFormatter = (number: number) => {
+    const size = number.toString().length;
+
+    // reserve 3 cells for points
+    return size <= 2
+      ? ["", ...(size === 1 ? [""] : []), ...number.toString().split("")]
+      : number.toString().split("");
+  };
+
+  if (!answers) return null;
 
   return (
     <div className={`${dottedFont.className} ${styles.container}`}>
       <div className={styles.data}>
-        {answers &&
-          answers.map((el: any, i: number) => {
-            const answer = el.answer.split("");
-            const points =
-              el.points.toString().length === 1
-                ? ["", ...el.points.toString()]
-                : el.points.toString().split("");
+        {answers.map((el: any, i: number) => {
+          const answer = el.answer.split("");
+          const points = numberFormatter(el.points);
 
-            return (
-              <div className={styles.item} key={i}>
-                <p>{i + 1}</p>
+          return (
+            <div className={styles.item} key={i}>
+              <p>{i + 1}</p>
 
-                {/* answer revealed */}
-                {show.includes(i + 1) && (
-                  <>
-                    <div className={styles.answer}>
-                      {answer.map((word: string, i: number) => (
-                        <p key={i}>{word}</p>
-                      ))}
-                    </div>
-
-                    <div className={styles.points}>
-                      {points.map((word: string, i: number) => (
-                        <p key={i}>{word}</p>
-                      ))}
-                    </div>
-                  </>
-                )}
-
-                {/* answer hidden */}
-                {!show.includes(i + 1) && (
-                  <div className={styles.dots}>
-                    {Array(17)
-                      .fill("...")
-                      .map((cell: string, i: number) => (
-                        <p key={i}>{cell}</p>
-                      ))}
+              {(show.includes(i + 1) && (
+                // show answer
+                <>
+                  <div className={styles.answer}>
+                    {answer.map((word: string, i: number) => {
+                      return <p key={i}>{word}</p>;
+                    })}
                   </div>
-                )}
-              </div>
-            );
-          })}
+
+                  <div className={styles.points}>
+                    {points.map((word: string, i: number) => {
+                      return <p key={i}>{word}</p>;
+                    })}
+                  </div>
+                </>
+              )) || (
+                // show dots
+                <div className={styles.dots}>
+                  {Array(17)
+                    .fill("...")
+                    .map((cell: string, i: number) => {
+                      return <p key={i}>{cell}</p>;
+                    })}
+                </div>
+              )}
+            </div>
+          );
+        })}
+
+        <div className={styles.pointsAmount}>
+          <div>
+            {"SUMA".split("").map((el: string, i: number) => {
+              return <p key={i}>{el}</p>;
+            })}
+          </div>
+
+          <div>
+            {numberFormatter(pointsAmount.current).map(
+              (el: string, i: number) => {
+                return <p key={i}>{el}</p>;
+              }
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
