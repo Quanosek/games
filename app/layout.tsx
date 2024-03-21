71d19e00a1b764d3375088e80344885c4f89f652
quanosek <kubaklalo@gmail.com>
2024-03-21 17:31:38 +0100

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import type { ReactNode } from "react";
 
 import "the-new-css-reset/css/reset.css";
 import "./globals.scss";
@@ -30,24 +31,20 @@ export const metadata: Metadata = {
   title: "Pokój gier / klalo.pl",
   description:
     "Przeglądarkowe wersje znanych gier towarzyskich i rodzinnych gier telewizyjnych przeznaczone do gry w większym gronie osób.",
+
   icons: {
-    icon: "/favicons/game_die.svg",
+    icon: ["/favicons/game_die.ico", "/favicons/game_die.svg"],
+    apple: "/favicons/game_die-icon.png",
   },
 };
 
 // global viewport
 export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  themeColor: "black",
+  themeColor: "#000000",
 };
 
 // app project layout
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pl" className={Nexa.className}>
       <body>{children}</body>
