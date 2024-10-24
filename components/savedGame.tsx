181be9deccd8005db8984c136453091c5086a27b
quanosek <kubaklalo@gmail.com>
2024-10-25 00:16:28 +0200

    game save changing title

--- components/savedGame.tsx
+++ components/savedGame.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Game } from "next-auth";
@@ -15,27 +16,35 @@ export interface Params {
 }
 
 export default function SavedGameComponent({ type, data }: Params) {
+  const router = useRouter();
   const { data: session } = useSession();
   const user = session?.user;
 
   const [game, setGame] = useState<Game>();
   const [title, setTitle] = useState("Nowa gra");
+  const [loading, setLoading] = useState(true);
 
+  // on component load
   useEffect(() => {
     const localGame = JSON.parse(localStorage.getItem(`${type}`) || "{}");
-    if (!localGame.id) return;
+    if (!localGame.id) return setLoading(false);
 
     axios
       .get("/api/game", { params: { id: localGame.id } })
-      .then((res) => setGame(res.data.result));
+      .then((res) => {
+        setGame(res.data.result);
+        setTitle(res.data.result.title);
+      })
+      .catch((error) => toast.error(error.response.data.message))
+      .finally(() => setLoading(false));
   }, []);
 
-  if (!user) return null;
-
   const saveGame = async () => {
-    const request = { userId: user.id, type, title, data };
+    const request = { userId: user?.id, type, title, data };
     const response = await axios.post("/api/game", request);
+
     setGame(response.data.result);
+    setTitle(response.data.result.title);
 
     const localData = JSON.parse(localStorage.getItem(`${type}`) || "{}");
     const { id: _, ...params } = localData;
@@ -46,16 +55,28 @@ export default function SavedGameComponent({ type, data }: Params) {
   };
 
   const deleteGame = () => {
-    if (!game) return;
-
-    axios
-      .delete("/api/game/delete", { params: { id: game.id } })
-      .then(() => toast.success("Gra została usunięta"))
-      .catch((error) => toast.error(error.response.data.message));
+    if (game) {
+      axios
+        .delete("/api/game/delete", { params: { id: game.id } })
+        .then(() => toast.success("Gra została usunięta"))
+        .catch((error) => toast.error(error.response.data.message));
+    } else {
+      localStorage.removeItem(`${type}`);
+      router.refresh();
+      toast.success("Wyczyszczono formularz");
+    }
   };
 
+  if (!user) return null;
+
   return (
-    <div className={styles.savedGamePrompt}>
+    <div
+      className={styles.savedGamePrompt}
+      style={{
+        transform: loading ? "translateY(-150%)" : "translateY(0)",
+        transition: "transform 200ms ease-out",
+      }}
+    >
       <div className={styles.promptContainer}>
         <div>
           <button onClick={saveGame}>
@@ -80,24 +101,27 @@ export default function SavedGameComponent({ type, data }: Params) {
 
         <div className={styles.editDate}>
           {game && (
-            <p>
-              Ostatni zapis:{" "}
-              {`${new Date(game.createdAt).toLocaleDateString()}, ${new Date(
-                game.createdAt
-              ).toLocaleTimeString()}`}
-            </p>
-          )}
+            <>
+              <p>
+                Ostatni zapis:{" "}
+                {game &&
+                  `${new Date(game.createdAt).toLocaleDateString()}, ${new Date(
+                    game.createdAt
+                  ).toLocaleTimeString()}`}
+              </p>
 
-          {/* <button onClick={deleteGame}>
-            <Image
-              className="icon"
-              alt="exit"
-              src="/icons/close.svg"
-              width={18}
-              height={18}
-              draggable={false}
-            />
-          </button> */}
+              <button onClick={deleteGame}>
+                <Image
+                  className="icon"
+                  alt="exit"
+                  src="/icons/close.svg"
+                  width={18}
+                  height={18}
+                  draggable={false}
+                />
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
